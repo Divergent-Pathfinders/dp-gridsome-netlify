286f71364d29717a48fa71f77d56f2dcff7c262a
Matthew Bellringer <matthew@elumin.co.uk>
2021-07-14 18:15:15 +0100

    added NeltifyCMS

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,5 +6,26 @@
 
 module.exports = {
   siteName: 'Gridsome',
-  plugins: []
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link'
+    }
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'posts/**/*.md',
+        typeName: 'Post'
+      }
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
+      }
+    },
+  ]
 }
