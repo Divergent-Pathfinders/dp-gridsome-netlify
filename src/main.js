200a4b1b1256b013851bfe506a50ab858eea3d63
Matthew Bellringer <matthew@elumin.co.uk>
2021-07-15 15:22:32 +0100

    Added Bootstrap to main config

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@
 
 import DefaultLayout from '~/layouts/Default.vue'
 
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  // Enable Bootstrap View frontend framework
+  Vue.use(BootstrapVue)
 }
